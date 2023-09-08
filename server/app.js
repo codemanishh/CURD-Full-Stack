f6fa507b5a19d7020638aa019cc9ac6d15b1f3c4
manish <rrew1212@gmail.com>
2023-09-08 13:37:07 +0530

    db connection stablish

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-
+const connectToDB = require('./config/db'); 
 require("dotenv").config();
+
 
 const express = require("express");
 
 const app = express();
+// Initialising connection to DB
+connectToDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
